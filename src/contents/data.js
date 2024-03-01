3f58d895d4e583a6a36f79c7492a0a8a8c781523
Ogheneochuko <femioriodofe@gmail.com>
2024-03-01 18:42:11 +0100

    stackup modal

---
+++ src/contents/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+      "name": "Photo 1",
+      "url": "/images/image1.jpg",
+      "description": "Photo 1 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 2",
+      "url": "/images/image2.jpg",
+      "description": "Photo 2 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 3",
+      "url": "/images/image3.jpg",
+      "description": "Photo 3 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 4",
+      "url": "/images/image4.jpg",
+      "description": "Photo 4 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 5",
+      "url": "/images/image5.jpg",
+      "description": "Photo 5 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 6",
+      "url": "/images/image6.jpg",
+      "description": "Photo 6 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 7",
+      "url": "/images/image7.jpg",
+      "description": "Photo 7 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 8",
+      "url": "/images/image8.jpg",
+      "description": "Photo 8 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 9",
+      "url": "/images/image9.jpg",
+      "description": "Photo 9 description",
+      "date": "2024-02-23",
+      "type": "image/jpg",
+    }
+  ]
